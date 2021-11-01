aa141614c7297cfc576934a42923ab2620aaed6f
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-11-01 15:14:07 +0100

    Added: Next Excercise

---
+++ 25/4.1 Streichen eines Strings aus einer Zeichenkette/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 900
+
+int main() {
+    char string[MAX];
+    char removechar;
+    int deletions = 0, stringlen = 0;
+
+    printf("Geben Sie eine Kitchenette zum bearbeiten ein: ");
+    fgets(string, MAX, stdin);
+
+    printf("Geben Sie eine Zeichen zum löschen ein: ");
+    scanf("%c", &removechar);
+    printf("\n");
+
+    stringlen = strlen(string);
+
+    for (int i = 0; i < MAX ; ++i) {
+        if(string[i] == removechar) {
+            string[i] = 0;
+            deletions++;
+        }
+    }
+
+    printf("Der neue String lautet: \n"); {
+        for (int i = 0; i < stringlen - deletions + 1; ++i) {
+            if(string[i])
+                printf("%c", string[i]);
+        }
+    }
+
+    return 0;
+}
